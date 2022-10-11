070ab5ff064fb3c04c56088740dfc11d05129d44
Carlos Fernando <carlosf.s.x@hotmail.com>
2022-10-11 20:53:19 -0300

    Configuration of application

---
+++ src/main/java/Column.java
@@ -0,0 +1,17 @@
+public class Column {
+    private String nomeFuncionalidade;
+    private Double threshold;
+
+    public Column(String nomeFuncionalidade, Double threshold) {
+        this.nomeFuncionalidade = nomeFuncionalidade;
+        this.threshold = threshold;
+    }
+
+    public String getNomeFuncionalidade() {
+        return nomeFuncionalidade;
+    }
+
+    public Double getThreshold() {
+        return threshold;
+    }
+}
---
+++ src/main/java/MethodResponse.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class MethodResponse {
+    private List<String> names;
+
+    public MethodResponse(List<String> names) {
+        this.names = names;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+}
